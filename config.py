83564572853ae9f54803c2c4c899a06ea3f1d005
Rufio244 <thanvas9991@gmail.com>
2026-07-13 00:52:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,7 @@ MONGO_URI = "mongodb://localhost:27017/"
 DB_NAME = "nexus_s3_db"
 SECRET_KEY = "nexus_s3_2026_secure"
 ADMIN_PLAN = "PRANAI"
+# ⚙️ เพิ่มส่วนนี้ท้ายไฟล์
+MASTER_UNLOCK_CODE = "976437754S31346Neng087Tom144"
+LOCK_ALL_SYSTEM = False  # True = ล็อกทั้งระบบ, False = ทำงานปกติ
 
